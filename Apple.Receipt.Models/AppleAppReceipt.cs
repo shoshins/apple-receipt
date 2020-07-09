128e43208a64e9453a6bb8d629d340a720d25799
Jan Trejbal <jan.trejbal@aviationexam.com>
2020-07-09 13:55:40 +0200

    Change time convert mechanism

--- Apple.Receipt.Models/AppleAppReceipt.cs
+++ Apple.Receipt.Models/AppleAppReceipt.cs
@@ -199,7 +199,8 @@ namespace Apple.Receipt.Models
             {
                 return null;
             }
-            var dt = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds(milliseconds);
+
+            var dt = DateTimeOffset.FromUnixTimeMilliseconds(milliseconds).UtcDateTime;
             return dt;
         }
 
