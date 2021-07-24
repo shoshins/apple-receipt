3272f35e80b47f0f7f4d402f1635873591180a7f
Sergey Shoshin <sergey.shoshin@vmedia.ca>
2021-07-24 16:18:12 +0300

    #16 -> Extra Examples
    Web Examples. Configuration examples improved.

---
+++ Apple.Receipt.Example/SecretsConfig.cs
@@ -0,0 +1,9 @@
+namespace Apple.Receipt.Example
+{
+    public class AppConfig
+    {
+        public string AppleSharedKey { get; set;}  
+        public string AppleReceiptExample { get; set;}  
+        public string AppleAppId { get; set;}  
+    }
+}
