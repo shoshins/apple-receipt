4abec4017e6e6fd91bd9dec0573af44eb52d1ee3
Sergey Shoshin <sergey.shoshin@vmedia.ca>
2021-07-24 16:20:09 +0300

    #16 Extra Examples
    WeatherForecastController converted to the specific Example based one

---
+++ Apple.Receipt.Example/Controllers/AppleReceiptLibrariesExamplesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Apple.Receipt.Models;
+using Apple.Receipt.Parser.Services;
+using Apple.Receipt.Verificator.Models;
+using Apple.Receipt.Verificator.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Apple.Receipt.Example.Controllers
+{
+    [ApiController]
+    [Route("apple-receipt")]
+    public class AppleReceiptLibrariesExamplesController : ControllerBase
+    {
+        private readonly AppConfig _config;
+        private readonly IAppleReceiptVerificatorService _appleVerificationService;
+        private readonly IAppleReceiptParserService _appleParserService;
+
+        public AppleReceiptLibrariesExamplesController(AppConfig config, 
+            IAppleReceiptVerificatorService appleVerificationService, 
+            IAppleReceiptParserService appleParserService)
+        {
+            _config = config;
+            _appleVerificationService = appleVerificationService;
+            _appleParserService = appleParserService;
+        }
+
+        [HttpGet("verificator")]
+        public async Task<string> CheckVerificator()
+        {
+            AppleReceiptVerificationResult verificationResult = await _appleVerificationService.VerifyAppleReceiptAsync(_config.AppleReceiptExample);
+            return JsonConvert.SerializeObject(verificationResult);
+        }
+        
+        [HttpGet("parser")]
+        public string CheckParser()
+        {
+            var data = Convert.FromBase64String(_config.AppleReceiptExample);
+            AppleAppReceipt verificationResult = _appleParserService.GetAppleReceiptFromBytes(data);
+            return JsonConvert.SerializeObject(verificationResult);
+        }
+    }
+}
